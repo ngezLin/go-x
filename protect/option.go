1eb108ea7499f42a279bda59d68e8a2626d5cd1d
Dede Kurniawan <dede.kurniawan@amartha.com>
2024-12-07 21:40:17 +0700

    add protect

---
+++ protect/option.go
@@ -0,0 +1,19 @@
+package protect
+
+import (
+	"context"
+	"time"
+)
+
+type Options struct {
+	//base64 encoded
+	RemoteURL string
+	//base64 encoded
+	RemoteSecret string
+	//base64 encoded
+	RemoteKey string
+	DistintId string
+	Backoff   func() time.Duration
+	stopper   func(ctx context.Context) error
+	RemoteProvider
+}
